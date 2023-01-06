6ab08a9c83bc842c82e809dc6883455c922d1821
JuanQP <quinterosp.juan@gmail.com>
2023-01-05 23:13:50 -0300

    Create docker file for development

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,31 +1,35 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, './src')},
-      { find: '@features', replacement: path.resolve(__dirname, './src/features')},
-      { find: '@pages', replacement: path.resolve(__dirname, './src/pages')},
-    ]
-  },
-  server: {
-    proxy: {
-      '/api': {
-        // This will work with docker container
-        target: 'http://django:8000/',
-        changeOrigin: false,
-        secure: false,
-      },
-      '/media': {
-        // This will work with docker container
-        target: 'http://django:8000/',
-        changeOrigin: false,
-        secure: false,
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, './src')},
+        { find: '@features', replacement: path.resolve(__dirname, './src/features')},
+        { find: '@pages', replacement: path.resolve(__dirname, './src/pages')},
+      ]
+    },
+    server: {
+      proxy: {
+        '/api': {
+          // This will work with docker container
+          target: env.BACKEND_URL,
+          changeOrigin: false,
+          secure: false,
+        },
+        '/media': {
+          // This will work with docker container
+          target: env.BACKEND_URL,
+          changeOrigin: false,
+          secure: false,
+        },
+      }
     }
   }
 })
